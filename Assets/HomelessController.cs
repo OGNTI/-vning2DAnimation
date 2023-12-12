7be3c1677471d8cb5f17bc5b14e25503dd0e60f7
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2023-12-12 09:37:55 +0100

    uftjvhbkagsdg

--- Assets/HomelessController.cs
+++ Assets/HomelessController.cs
@@ -15,7 +15,7 @@ public class HomelessController : MonoBehaviour
     {
         float walkSpeed = 5 * Time.deltaTime;
 
-        // if (Input.GetAxisRaw("Fire1") > 0) animator.SetTrigger("Attack");
+        if (Input.GetAxisRaw("Fire1") > 0) animator.SetTrigger("Attack");
 
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
@@ -23,11 +23,7 @@ public class HomelessController : MonoBehaviour
         // animator.SetFloat("x", moveX);
         // animator.SetFloat("y", moveY);
 
-             if (moveX > 0 && Input.GetAxisRaw("Fire1") > 0) animator.Play("attackRight");
-        else if (moveX < 0 && Input.GetAxisRaw("Fire1") > 0) animator.Play("attackLeft");
-        else if (moveY > 0 && Input.GetAxisRaw("Fire1") > 0) animator.Play("attackUp");
-        else if (moveX < 0 && Input.GetAxisRaw("Fire1") > 0) animator.Play("attackDown");
-        else if (moveX > 0) animator.Play("walkRight");
+             if (moveX > 0) animator.Play("walkRight");
         else if (moveX < 0) animator.Play("walkLeft");
         else if (moveY > 0) animator.Play("walkUp");
         else if (moveY < 0) animator.Play("walkDown");
---
+++ Assets/SwordController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    void AnimationFinished()
+    {
+        bool g = true;
+    }
+}
